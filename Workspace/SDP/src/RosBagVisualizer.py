da3a6afb9569fdd4858cb25751d8e0bd5c9f0cdb
alan <agrija9@gmail.com>
2019-10-30 12:17:57 +0100

    added RosBagVisualizer

---
+++ Workspace/SDP/src/RosBagVisualizer.py
@@ -0,0 +1,14 @@
+# RosBagVisualizer.py
+
+# TODO: Consider huge inputs from divin's class
+# TODO: Consider pipeline for reading and plottin timeseries
+
+import matplotlib.pyplot as plt
+
+class Plotter():
+
+	def __init__():
+
+
+
+	def _ 
