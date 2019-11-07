5758a043cb9390355fb7e19cd205e6cb527b483b
rkutty1 <jupey1994@gmail.com>
2019-11-07 19:19:05 +0100

    webpage updated code

---
+++ Workspace/SDP/src/csv_to_chart.py
@@ -0,0 +1,3 @@
+# bubble chart as the landing graph or interaction (shows the strength of topic acording to time active :https://observablehq.com/@d3/bubble-chart
+# D3 tutorial(30 min) :https://scrimba.com/g/gd3js
+# 
