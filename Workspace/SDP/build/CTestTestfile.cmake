9819ec37a884fcbf972ad22c14a5da7ce2e85ca2
alan <agrija9@gmail.com>
2020-01-16 09:11:26 +0100

    Removed workspace folder

--- Workspace/SDP/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/d/SDP/src
-# Build directory: /home/d/SDP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
